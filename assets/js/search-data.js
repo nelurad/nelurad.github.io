6086ad17252f9b665302e125093ea45972e01836
nelurad <radpour@psy.fsu.edu>
2025-10-05 22:47:17 +0000

    Deploying to gh-pages from @ nelurad/nelurad.github.io@99cee32d42ce0d3a75ba9cd2d35d0546490c7f46 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%61%64%70%6F%75%72@%70%73%79.%66%73%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
