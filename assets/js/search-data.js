25961b121b76e48ab544493bafee4d70ab48def8
nelurad <radpour@psy.fsu.edu>
2025-10-05 23:21:23 +0000

    Deploying to gh-pages from @ nelurad/nelurad.github.io@fec8f5c776b58b90fbbde6528e49f2a01de37302 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,16 +405,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-began-my-phd-in-cognition-and-cognitive-neuroscience-at-florida-state-university",
+          title: 'I began my PhD in Cognition and Cognitive Neuroscience at Florida State University!...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
